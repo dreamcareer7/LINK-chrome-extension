913066e1f875f286bbe69b011c762da48d431453
dreamcareer7 <dreamcareer070@gmail.com>
2020-12-24 16:55:20 +0530

    popup.html and popup.js files added to git

---
+++ popup.js
@@ -0,0 +1,8 @@
+// function getAllRequirements() {
+//   chrome.storage.sync.get(null, (data) => {
+//     console.log(JSON.parse(data));
+//   });
+// }
+var console = chrome.extension.getBackgroundPage().console;
+
+console.log('HEllo')
